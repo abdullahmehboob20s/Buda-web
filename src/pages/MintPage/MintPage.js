2caed8a38f063f9ee52a1ef2e756149c4e0686b8
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-04-04 13:09:55 +0500

    Mint page ready

--- src/pages/MintPage/MintPage.js
+++ src/pages/MintPage/MintPage.js
@@ -1,19 +1,46 @@
 import useMediaQuery from "hooks/useMediaQuery";
 import Navbar from "layouts/Navbar/Navbar";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MintPage.module.css";
+
+import { ImMinus, ImPlus } from "react-icons/im";
+import { CgFormatSlash } from "react-icons/cg";
+
+const Colon = () => {
+  return (
+    <div className={`${styles.colon}`}>
+      <div className="fs-30px white">.</div>
+      <div className="fs-30px white">.</div>
+    </div>
+  );
+};
+
+const Timer = ({ time, title }) => {
+  return (
+    <div className={`${styles.timer}`}>
+      <div className={`${styles.timerBox_wrapper} mb-10px`}>
+        <div className={`${styles.timer_box} fs-40px text-center black`}>
+          {time}
+        </div>
+      </div>
+      <p className="text-center fs-18px uppercase">{title}</p>
+    </div>
+  );
+};
 
 function MintPage() {
   const isBellow1000px = useMediaQuery("(max-width : 1000px)");
   const isBellow600px = useMediaQuery("(max-width : 600px)");
+  let [counter, setCounter] = useState(1);
+
   return (
     <div className="container-wrapper">
-      <div className="mb-50px">
+      <div className="">
         <Navbar />
       </div>
 
-      <div className={`${styles.page_content}`}>
-        <div className={isBellow1000px ? "mb-40px" : "mb-80px"}>
+      <div className={`${styles.page_content} py-50px`}>
+        <div className={isBellow1000px ? "mb-20px" : "mb-40px"}>
           <h1
             className={`text-center font-mouse white ${
               isBellow1000px ? "fs-50px" : "fs-100px"
@@ -30,10 +57,57 @@ function MintPage() {
           </p>
 
           <button
-            className={`${styles.connect_btn} yellow-btn white fs-28px text-center pointer`}
+            className={`${styles.connect_btn} yellow-btn white fs-20px text-center pointer`}
           >
             CONNECT WALLET
           </button>
+        </div>
+
+        <div>
+          <h1 className="text-center fs-40px white mb-40px">
+            JANUARY 21ST 1PM EST
+          </h1>
+
+          <div className={`${styles.timer_container} mb-50px`}>
+            <Timer time="13" title="Days" />
+            <Colon />
+            <Timer time="12" title="Hours" />
+            <Colon />
+            <Timer time="21" title="Minutes" />
+            <Colon />
+            <Timer time="34" title="Seconds" />
+          </div>
+
+          <div
+            className={`${styles.maximum_mint} fs-30px white text-center weight-4 mb-25px`}
+          >
+            {counter} <CgFormatSlash /> 10000
+          </div>
+          <div
+            className={`${styles.whitelist_mint} fs-30px black text-center uppercase mb-25px`}
+          >
+            WHITELIST MINT (MAX 1)
+          </div>
+          <button
+            className={`${styles.mint_btn} fs-24px white text-center btn-red mb-30px`}
+          >
+            Mint Now
+          </button>
+
+          <div className={`${styles.counter}`}>
+            <button
+              className="pointer"
+              onClick={() => setCounter(counter < 2 ? counter : counter - 1)}
+            >
+              <ImMinus color="white" size={30} />
+            </button>
+            <div className={`${styles.counter_box} fs-30px black`}>
+              {counter}
+            </div>
+            <button className="pointer" onClick={() => setCounter(counter + 1)}>
+              <ImPlus color="white" size={30} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
