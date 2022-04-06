1fc27ef958c13567745c17644f51057f680a23fb
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-04-06 19:32:34 +0500

    Added Footer in Mint Page

--- src/pages/MintPage/MintPage.js
+++ src/pages/MintPage/MintPage.js
@@ -8,6 +8,7 @@ import { CgFormatSlash } from "react-icons/cg";
 import YellowBtn from "components/Buttons/YellowBtn/YellowBtn";
 import RedButton from "components/Buttons/RedButton/RedButton";
 import { useNavigate } from "react-router-dom";
+import Footer from "layouts/Footer/Footer";
 
 const Colon = () => {
   return (
@@ -88,107 +89,121 @@ function MintPage() {
   });
 
   return (
-    <div className={`${styles.mint_page}`}>
+    <div>
       <div className="">
         <Navbar />
       </div>
-      <div className={`${styles.mint_page_container} container-wrapper`}>
-        <div className={`${styles.page_content} py-50px`}>
-          <div className={isBellow1000px ? "mb-20px" : "mb-40px"}>
-            <h1
-              className={`text-center font-mouse white ${
-                isBellow1000px ? "fs-50px" : "fs-100px"
-              } weight-4 mb-20px`}
-            >
-              Mint Now.
-            </h1>
-            <p
-              className={`text-center white ${
-                isBellow1000px ? "fs-18px" : "fs-28px"
-              } uppercase mb-30px`}
-            >
-              WHITELIST MINTS WILL BE PRICED AT 0.069420 ETH
-            </p>
 
-            <YellowBtn
-              title="CONNECT WALLET"
-              fontSize={isBellow1000px ? "fs-16px" : "fs-20px"}
-              className={styles.connect_btn}
-              onClick={() => navigate("/connect-wallet")}
-            />
-          </div>
-
-          <div>
-            <h1
-              className={`text-center ${
-                isBellow1000px ? "fs-26px" : "fs-40px"
-              } white mb-40px`}
-            >
-              JANUARY 21ST 1PM EST
-            </h1>
-            <div className={`${styles.timer_container} mb-50px`}>
-              <Timer time={timerDays} title={isBellow600px ? "Days" : "Days"} />
-              <Colon />
-              <Timer
-                time={timerHours}
-                title={isBellow600px ? "Hrs" : "Hours"}
-              />
-              <Colon />
-              <Timer
-                time={timerMinutes}
-                title={isBellow600px ? "Min" : "Minutes"}
-              />
-              <Colon />
-              <Timer
-                time={timerSeconds}
-                title={isBellow600px ? "Sec" : "Seconds"}
-              />
-            </div>
-            <div
-              className={`${styles.maximum_mint} ${
-                isBellow1000px ? "fs-20px" : "fs-30px"
-              } white text-center weight-4 mb-25px`}
-            >
-              {counter} <CgFormatSlash /> 10000
-            </div>
-            <div
-              className={`${styles.whitelist_mint} ${
-                isBellow1000px ? "fs-20px" : "fs-30px"
-              } black text-center uppercase mb-25px`}
-            >
-              WHITELIST MINT (MAX 1)
-            </div>
-
-            <RedButton
-              title="Mint Now"
-              fontSize={isBellow1000px ? "fs-16px" : "fs-20px"}
-              className={`${styles.connect_btn} mb-25px`}
-            />
-
-            <div className={`${styles.counter}`}>
-              <button
-                className="pointer"
-                onClick={() => setCounter(counter < 2 ? counter : counter - 1)}
+      <div
+        className={`${styles.mint_page} ${
+          isBellow600px ? "mb-50px" : "mb-100px"
+        }`}
+      >
+        <div className={`${styles.mint_page_container} container-wrapper `}>
+          <div className={`${styles.page_content} py-50px`}>
+            <div className={isBellow1000px ? "mb-20px" : "mb-40px"}>
+              <h1
+                className={`text-center font-mouse white ${
+                  isBellow1000px ? "fs-50px" : "fs-100px"
+                } weight-4 mb-20px`}
               >
-                <ImMinus color="white" size={isBellow1000px ? 20 : 30} />
-              </button>
-              <div
-                className={`${styles.counter_box} ${
-                  isBellow1000px ? "fs-20px" : "fs-30px"
-                } black`}
+                Mint Now.
+              </h1>
+              <p
+                className={`text-center white ${
+                  isBellow1000px ? "fs-18px" : "fs-28px"
+                } uppercase mb-30px`}
               >
-                {counter}
+                WHITELIST MINTS WILL BE PRICED AT 0.069420 ETH
+              </p>
+
+              <YellowBtn
+                title="CONNECT WALLET"
+                fontSize={isBellow1000px ? "fs-16px" : "fs-20px"}
+                className={styles.connect_btn}
+                onClick={() => navigate("/connect-wallet")}
+              />
+            </div>
+
+            <div>
+              <h1
+                className={`text-center ${
+                  isBellow1000px ? "fs-26px" : "fs-40px"
+                } white mb-40px`}
+              >
+                JANUARY 21ST 1PM EST
+              </h1>
+              <div className={`${styles.timer_container} mb-50px`}>
+                <Timer
+                  time={timerDays}
+                  title={isBellow600px ? "Days" : "Days"}
+                />
+                <Colon />
+                <Timer
+                  time={timerHours}
+                  title={isBellow600px ? "Hrs" : "Hours"}
+                />
+                <Colon />
+                <Timer
+                  time={timerMinutes}
+                  title={isBellow600px ? "Min" : "Minutes"}
+                />
+                <Colon />
+                <Timer
+                  time={timerSeconds}
+                  title={isBellow600px ? "Sec" : "Seconds"}
+                />
               </div>
-              <button
-                className="pointer"
-                onClick={() => setCounter(counter + 1)}
+              <div
+                className={`${styles.maximum_mint} ${
+                  isBellow1000px ? "fs-20px" : "fs-30px"
+                } white text-center weight-4 mb-25px`}
               >
-                <ImPlus color="white" size={isBellow1000px ? 20 : 30} />
-              </button>
+                {counter} <CgFormatSlash /> 10000
+              </div>
+              <div
+                className={`${styles.whitelist_mint} ${
+                  isBellow1000px ? "fs-20px" : "fs-30px"
+                } black text-center uppercase mb-25px`}
+              >
+                WHITELIST MINT (MAX 1)
+              </div>
+
+              <RedButton
+                title="Mint Now"
+                fontSize={isBellow1000px ? "fs-16px" : "fs-20px"}
+                className={`${styles.connect_btn} mb-25px`}
+              />
+
+              <div className={`${styles.counter}`}>
+                <button
+                  className="pointer"
+                  onClick={() =>
+                    setCounter(counter < 2 ? counter : counter - 1)
+                  }
+                >
+                  <ImMinus color="white" size={isBellow1000px ? 20 : 30} />
+                </button>
+                <div
+                  className={`${styles.counter_box} ${
+                    isBellow1000px ? "fs-20px" : "fs-30px"
+                  } black`}
+                >
+                  {counter}
+                </div>
+                <button
+                  className="pointer"
+                  onClick={() => setCounter(counter + 1)}
+                >
+                  <ImPlus color="white" size={isBellow1000px ? 20 : 30} />
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 }
